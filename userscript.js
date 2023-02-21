f322411d14dc91d154e2f71d1eee6d7c34d28ae9
Khaled <spkhalad@gmail.com>
2023-02-21 17:46:36 +0600

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Youtube Auto "I understand and wish to proceed"
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Auto click the "I understand and wish to proceed" in age-restricted videos.
+// @author       khaled-0
+// @match        https://www.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(function() {
+        const consentBtn = document.querySelector("[aria-label=\"I understand and wish to proceed\"]");
+        if(consentBtn == null) return;
+        consentBtn.click();
+        consentBtn.remove();
+    }, 10)();
+})();
